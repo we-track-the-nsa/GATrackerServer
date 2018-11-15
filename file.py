0f6555b820a04f265e9546565c8d60d3bc294c3b
VijitSingh97 <vijit.singh@mavs.uta.edu>
2018-11-15 11:47:11 -0600

    shell script

--- file.py
+++
@@ -1,7 +0,0 @@
-f = open("Twitter_Accounts","r")
-
-if f.mode == "r":
-    contents = f.read().splitlines()
-    
-for content in contents:
-        print(content)
--- startCollection.sh
+++ startCollection.sh
@@ -3,5 +3,4 @@
 mkdir output
 mkdir error
 
-python NSATwitter.py 1>output/NSAoutput.log 2>error/NSAerror.log &
-python WHTwitter.py 1>output/WHoutput.log 2>error/WHerror.log &
+python twitterToFirebase.py 1>output/twitterOut.log 2>error/twitterToErr.log &
